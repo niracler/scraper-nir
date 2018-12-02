e41292a70cf290477575197ea2ebd19073eebaad
niracler <niracler@niracler-pc>
2018-12-02 21:34:20 +0800

    终于能够优雅地解决存储数据到mysql数据库的问题了

---
+++ scraper/scraper/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, Column, Table, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Integer, SmallInteger, String, Date, DateTime, Float, Boolean, Text, LargeBinary)
+from sqlalchemy.orm import sessionmaker
+from scrapy.utils.project import get_project_settings
+
+DeclarativeBase = declarative_base()
+
+
+def db_connect():
+    """
+    Performs database connection using database settings from settings.py.
+    Returns sqlalchemy engine instance
+    """
+    return create_engine(get_project_settings().get("CONNECTION_STRING"))
+
+
+def create_table(engine):
+    DeclarativeBase.metadata.create_all(engine)
+
+
+class Article(DeclarativeBase):
+    __tablename__ = "article"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column('title', String(200))
+
--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -5,8 +5,41 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from sqlalchemy.orm import sessionmaker
+from .models import Article, db_connect, create_table
+
 
 class ScraperPipeline(object):
     def process_item(self, item, spider):
         return item
 
+
+class ArticlePipeline(object):
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates deals table.
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        """
+        Save deals in the database.
+        This method is called for every item pipeline component.
+        """
+        session = self.Session()
+        article = Article()
+        article.title = item["title"]
+
+        try:
+            session.add(article)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+        return item
